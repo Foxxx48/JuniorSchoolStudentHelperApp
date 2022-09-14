bdd48e9a3d7a4055bd47e77ed14cc1d084c436d4
Foxxx48 <Foxxdim@mail.ru>
2022-09-14 17:05:14 +0300

    add functions in ChooseLevelFragment, GameFragment, WelcomeFragment

--- app/src/main/java/com/fox/juniorschoolstudenthelperapp/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/fox/juniorschoolstudenthelperapp/presentation/ChooseLevelFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.fox.juniorschoolstudenthelperapp.R
 import com.fox.juniorschoolstudenthelperapp.databinding.FragmentChooseLevelBinding
+import com.fox.juniorschoolstudenthelperapp.domain.entity.Level
 
 
 class ChooseLevelFragment : Fragment() {
@@ -19,8 +21,36 @@ class ChooseLevelFragment : Fragment() {
     ): View? {
         _binding = FragmentChooseLevelBinding.inflate(layoutInflater, container, false)
         return binding.root
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            buttonLevelTest.setOnClickListener {
+                launchGameFragment(Level.TEST)
+            }
+
+            buttonLevelEasy.setOnClickListener {
+                launchGameFragment(Level.EASY)
+            }
+
+            buttonLevelNormal.setOnClickListener {
+                launchGameFragment(Level.MEDIUM)
+            }
+
+            buttonLevelHard.setOnClickListener {
+                launchGameFragment(Level.HARD)
+            }
+        }
+    }
+
+    private fun launchGameFragment(level: Level) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFragment.newInstance(level))
+            .addToBackStack(null)
+            .commit()
+    }
 
 
     override fun onDestroyView() {
@@ -29,7 +59,7 @@ class ChooseLevelFragment : Fragment() {
     }
 
     companion object {
-        fun  newInstance(): ChooseLevelFragment {
+        fun newInstance(): ChooseLevelFragment {
             return ChooseLevelFragment()
         }
     }
--- app/src/main/java/com/fox/juniorschoolstudenthelperapp/presentation/GameFragment.kt
+++ app/src/main/java/com/fox/juniorschoolstudenthelperapp/presentation/GameFragment.kt
@@ -5,9 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.fox.juniorschoolstudenthelperapp.R
 import com.fox.juniorschoolstudenthelperapp.databinding.FragmentGameBinding
-import java.lang.RuntimeException
+import com.fox.juniorschoolstudenthelperapp.domain.entity.Level
 
 
 class GameFragment : Fragment() {
@@ -15,16 +14,49 @@ class GameFragment : Fragment() {
     private val binding
         get() = _binding ?: throw RuntimeException("GameFragmentBinding == null")
 
+    private lateinit var level: Level
+
+//    private lateinit var gameRepository: GameRepository
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+//        val newGameSettings = gameRepository.getGameSettings(level)
+
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentGameBinding.inflate(layoutInflater, container, false)
         return binding.root
+
+    }
+
+
+
+
+
+   private fun parseArgs() {
+        level = requireArguments().getSerializable(KEY_LEVEL) as Level
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        const val KEY_LEVEL = "level"
+
+        fun newInstance(level: Level): GameFragment {
+            return GameFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_LEVEL, level)
+                }
+            }
+        }
     }
 }
